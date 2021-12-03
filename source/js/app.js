108f453bae27b5b9ddf40eb5e3e628d7de18cd17
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2021-12-03 21:15:56 +0300

    [module3-task1] index.html: из page-header выделен элемент slogan.

--- source/js/app.js
+++ source/js/app.js
@@ -3,3 +3,13 @@ function toggleHidden(className) {
     .getElementsByClassName(className)[0]
   menu.classList.toggle("hidden")
 }
+
+function toggleMenu() {
+  toggleHidden("menu-content-popup")
+  toggleHidden("page-header__menu-button--open")
+  toggleHidden("page-header__menu-button--close")
+  const pageHeader = document
+    .getElementsByClassName("page-header")[0]
+  pageHeader.classList.toggle("page-header--dark")
+  pageHeader.classList.toggle("page-header--light")
+}
